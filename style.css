8aef517941b0df2a12f57b65a20da0f94e2dcbda
EugenSergeev <flashser@yandex.ru>
2019-09-18 19:15:21 +0300

    create
    Копирование проекта и первый коммит

---
+++ style.css
@@ -0,0 +1,128 @@
+@keyframes errors {
+    from {
+      margin-top: -5px;
+      opacity: .1; 
+    }
+  
+    to {
+      margin-top: 10px;
+      opacity: 1;
+    }
+}
+
+html {
+    font-family: Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+}
+
+.container {
+    max-width: 600px;
+    margin: auto;
+}
+
+.header {
+    max-width: 600px;
+    height: 80px;
+    margin: auto;
+    display: flex;
+    align-items: center;
+}
+
+.header__logo {
+    width: 150px;
+}
+
+.header__logo-sub {
+    margin-left: 10px;
+    margin-bottom: 25px;
+    font-size: 0.6em;
+}
+
+.form {
+    max-width: 600px;
+    margin: auto;
+}
+
+.block {
+    margin-top: 60px;
+    border-radius: 3px;
+  }
+
+.cover {
+    display: flex;
+    align-items: center;
+    margin-top: 30px;
+    margin-bottom: 20px;
+  }
+
+.cover__image {
+    width: 110px;
+    min-width: 110px;
+    height: 110px;
+    background-image: url(https://pictures.s3.yandex.net/frontend-developer/dom_bom/cover.jpg);
+    background-size: cover;
+    background-position: center;
+    border-radius: 50%;
+}
+  
+.cover__heading {
+    margin-left: 30px;
+    font-size: 2.5em;
+    line-height: 1;
+}
+
+.form__field {
+	margin-bottom: 36px;
+}
+
+.form__fieldset {
+    border: none;
+    margin: 0;
+    padding: 0;
+}
+
+.form__error {
+    display: none;
+    position: absolute;
+    margin-top: 10px;
+	color: #df4b41;
+	text-align: left;
+	font-size: 14px;
+    animation: errors .1s ease-in-out;
+}
+
+.form__input {
+    display: block;
+    width: 100%;
+    border: none;
+	border-radius: 2px;
+	border-bottom: 2px solid #ccc;
+	padding: 10px 0;
+    box-sizing: border-box;
+    font-size: 16px;
+}
+
+.form__button {
+    width: 200px;
+    height: 48px;
+    font-weight: bold;
+    font-size: 14px;
+    border-radius: 3px;
+    border: none;
+    text-align: center;
+    cursor: pointer;
+	background-color: #ffdb4d;
+	color: #000;
+}
+
+.form__input:valid:not(:placeholder-shown) {
+	border-color: #ffdb4d;
+}
+
+.form__input:invalid:not(:placeholder-shown) {
+	border-color: #df4b41;
+}
+
+.form__input:invalid:not(:placeholder-shown) + .form__error {
+	display: block;
+}
